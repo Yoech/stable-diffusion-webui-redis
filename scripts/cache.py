ed8434bbae03a315b7fa682ced66ba8dab554b0c
mowen <mowen@126.com>
2023-05-30 10:19:02 +0000

    up

--- scripts/cache.py
+++ scripts/cache.py
@@ -6,13 +6,13 @@ import redis
 import modules.scripts as scripts
 import gradio as gr
 
+redis_save = os.environ.get('REDIS_SAVE', False)
 redis_host = os.environ.get('REDIS_HOST', '127.0.0.1')
 redis_port = os.environ.get('REDIS_PORT', 6379)
 redis_db = os.environ.get('REDIS_DB', 0)
 redis_auth = os.environ.get('REDIS_AUTH', '')
 
 def get_collection(host: str = '127.0.0.1', port: int = 6379, db: int = 0, password: str = ''):
-    print(f"get_collection--->host[{host}].port[{port}].db[{db}].password[{password}]")
     conn_pool = redis.ConnectionPool(host=host, port=port, db=db, password=password, max_connections=10)
     return redis.Redis(connection_pool=conn_pool)
 
@@ -25,15 +25,20 @@ class Scripts(scripts.Script):
         return scripts.AlwaysVisible
 
     def ui(self, is_img2img):
-        checkbox_save_to_redis = gr.inputs.Checkbox(label="Save to Redis", default=True)
-        host = gr.inputs.Textbox(label="host", default=redis_host)
-        port = gr.inputs.Textbox(label="port", default=redis_port)
-        db = gr.inputs.Textbox(label="db", default=redis_db)
-        password = gr.inputs.Textbox(label="password", default=redis_auth)
-        return [checkbox_save_to_redis, host, port, db, password]
+        import modules.ui
+        print(f"Redis Configure save: {redis_save}")
+        with gr.Group():
+            with gr.Accordion("Redis Configure", open=True):
+                with gr.Row():
+                    checkbox_save_to_redis = gr.Checkbox(label="Enable", default=redis_save)
+                    host = gr.inputs.Textbox(label="host", default=redis_host)
+                    port = gr.inputs.Textbox(label="port", default=redis_port)
+                    db = gr.inputs.Textbox(label="db", default=redis_db)
+                    password = gr.inputs.Textbox(label="password", default=redis_auth)
+        return checkbox_save_to_redis, host, port, db, password
 
     def postprocess(self, p, processed, checkbox_save_to_redis, host, port, db, password):
-        print(f"host[{host}].port[{port}].db[{db}].password[{password}]")
+        print(f"postprocess ---> Save2Redis[{redis_save}]")
         collection = get_collection(host, port, db, password) if checkbox_save_to_redis else None
         if collection is None:
             return True
@@ -53,8 +58,5 @@ class Scripts(scripts.Script):
         input_dict = dict(item.split(": ") for item in str(info).split(", "))
         collection.hmset("RS:B:100:info", {"info": info})
 
-        # processed.info = None
-        # processed.images = None
-        # collection.hmset("RS:B:100:processed", {"processed": processed})
-
+        print(f"postprocess ---> Save2Redis.completed!")
         return True
