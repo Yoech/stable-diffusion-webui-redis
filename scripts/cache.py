7f14ee2a21343896f13a7686d80e59b33feb2b91
CC <mowen@126.com>
2023-06-01 11:14:49 +0800

    Update cache.py

--- scripts/cache.py
+++ scripts/cache.py
@@ -13,6 +13,7 @@ redis_port = int(os.environ.get('REDIS_PORT', 6379))
 redis_db = int(os.environ.get('REDIS_DB', 0))
 redis_auth = os.environ.get('REDIS_AUTH', '')
 
+
 def get_collection(host: str = '127.0.0.1', port: int = 6379, db: int = 0, password: str = ''):
     conn_pool = redis.ConnectionPool(host=host, port=port, db=db, password=password, max_connections=10)
     return redis.Redis(connection_pool=conn_pool)
@@ -31,20 +32,20 @@ class Scripts(scripts.Script):
 
         with gr.Group():
             with gr.Accordion("Redis Configure", open=True):
-                #with gr.Box():
+                # with gr.Box():
+                with gr.Row():
+                    # checkbox_save_to_redis = gr.inputs.Checkbox(label="Enable", default=redis_save)
+                    checkbox_save_to_redis = gr.Checkbox(label="Enable", value=redis_save)
+                with gr.Group():
                     with gr.Row():
-                        #checkbox_save_to_redis = gr.inputs.Checkbox(label="Enable", default=redis_save)
-                        checkbox_save_to_redis = gr.Checkbox(label="Enable", value=redis_save)
-                    with gr.Group():
-                        with gr.Row():
-                            #host = gr.inputs.Textbox(label="Host", default=redis_host)
-                            host = gr.Textbox(label="Host", value="Host Address")
-                            #port = gr.inputs.Textbox(label="Port", default=redis_port)
-                            port = gr.Textbox(label="Port", value="what")
-                            #db = gr.inputs.Textbox(label="db", default=redis_db)
-                            db = gr.Textbox(label="Db", value=redis_db)
-                            #password = gr.inputs.Textbox(label="Password", default=redis_auth)
-                            password = gr.Textbox(label="Password", value=redis_auth)
+                        # host = gr.inputs.Textbox(label="Host", default=redis_host)
+                        host = gr.Textbox(label="Host", value="Host Address")
+                        # port = gr.inputs.Textbox(label="Port", default=redis_port)
+                        port = gr.Textbox(label="Port", value="what")
+                        # db = gr.inputs.Textbox(label="db", default=redis_db)
+                        db = gr.Textbox(label="Db", value=redis_db)
+                        # password = gr.inputs.Textbox(label="Password", default=redis_auth)
+                        password = gr.Textbox(label="Password", value=redis_auth)
         return [checkbox_save_to_redis, host, port, db, password]
 
     def postprocess(self, p, processed, checkbox_save_to_redis, host, port, db, password):
@@ -58,16 +59,21 @@ class Scripts(scripts.Script):
             return True
 
         print(f"------------>images[{len(processed.images)}].seeds[{len(processed.all_seeds)}].subseeds[{len(processed.all_subseeds)}]")
+
+        # opts.return_grid==true
+        if len(processed.images) == len(processed.all_seeds) + 1:
+            processed.images = processed.images[:1]
+            print(f"------------>processed.images.length resize")
+
         for i in range(len(processed.images)):
             image = processed.images[i]
             buffer = BytesIO()
             image.save(buffer, "png")
             image_bytes = buffer.getvalue()
-            base64_image = base64.b64encode(image_bytes).decode('ascii')
-            # seed = processed.all_seeds[i]
-            # subseed = processed.all_subseeds[i]
-            # print(f"image[{i}].seeds={seed}.subseed={subseed}.bytes_size={len(image_bytes)}.base64_size={len(base64_image)}")
-            print(f"image[{i}].bytes_size={len(image_bytes)}.base64_size={len(base64_image)}.last_bytes=[{image_bytes[len(image_bytes)-20:len(image_bytes)-12].hex(' ')}]")
+            # base64_image = base64.b64encode(image_bytes).decode('ascii')
+            seed = processed.all_seeds[i]
+            subseed = processed.all_subseeds[i]
+            print(f"image[{i}].seeds={seed}.subseed={subseed}.bytes_size={len(image_bytes)}.first_bytes=[{image_bytes[:16].hex(' ')}].last_bytes=[{image_bytes[len(image_bytes) - 20:len(image_bytes) - 12].hex(' ')}]")
             # collection.hmset("RS:B:100:image", {"image": base64_image})
             # collection.hmset("RS:B:100:image:" + str(i), {str(seed): image_bytes})
 
