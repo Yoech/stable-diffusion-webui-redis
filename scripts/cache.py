c2ddf60be0f964e8e77d3e0dc6f2501145e3ddbe
mowen <mowen@126.com>
2023-06-01 08:55:19 +0000

    up

--- scripts/cache.py
+++ scripts/cache.py
@@ -12,7 +12,7 @@ redis_host = os.environ.get('REDIS_HOST', '127.0.0.1')
 redis_port = int(os.environ.get('REDIS_PORT', 6379))
 redis_db = int(os.environ.get('REDIS_DB', 0))
 redis_auth = os.environ.get('REDIS_AUTH', '')
-
+redis_prefix = os.environ.get('REDIS_PREFIX', '')
 
 def get_collection(host: str = '127.0.0.1', port: int = 6379, db: int = 0, password: str = ''):
     conn_pool = redis.ConnectionPool(host=host, port=port, db=db, password=password, max_connections=10)
@@ -29,33 +29,32 @@ class Scripts(scripts.Script):
     def ui(self, is_img2img):
         import modules.ui
         print(f"Redis Configure save: {redis_save}")
+        print(f"redis_host=>{redis_host}")
+        print(f"redis_port=>{redis_port}")
+        print(f"redis_db=>{redis_db}")
+        print(f"redis_auth=>{redis_auth}")
+        print(f"redis_prefix=>{redis_prefix}")
 
         with gr.Group():
             with gr.Accordion("Redis Configure", open=True):
-                # with gr.Box():
                 with gr.Row():
-                    # checkbox_save_to_redis = gr.inputs.Checkbox(label="Enable", default=redis_save)
-                    checkbox_save_to_redis = gr.Checkbox(label="Enable", value=redis_save)
+                    checkbox_save_to_redis = gr.Checkbox(label="enable", value=bool(redis_save))
                 with gr.Group():
                     with gr.Row():
-                        # host = gr.inputs.Textbox(label="Host", default=redis_host)
-                        host = gr.Textbox(label="Host", value=redis_host)
-                        # port = gr.inputs.Textbox(label="Port", default=redis_port)
-                        port = gr.Textbox(label="Port", value=redis_port)
-                        # db = gr.inputs.Textbox(label="db", default=redis_db)
-                        db = gr.Textbox(label="Db", value=redis_db)
-                        # password = gr.inputs.Textbox(label="Password", default=redis_auth)
-                        password = gr.Textbox(label="Password", value=redis_auth)
+                        host = gr.Textbox(label="redis_host", value=str(redis_host))
+                        port = gr.Textbox(label="redis_port", value=str(redis_port))
+                        db = gr.Textbox(label="redis_db", value=str(redis_db))
+                        pwd = gr.Textbox(label="redis_auth", value=str(redis_auth))
                     with gr.Row():
-                        prefix = gr.Textbox(label="Prefix", value="RS:B:")
-        return [checkbox_save_to_redis, host, port, db, password, prefix]
+                        prefix = gr.Textbox(label="redis_prefix", value=str(redis_prefix))
+        return [checkbox_save_to_redis, host, port, db, pwd, prefix]
 
-    def postprocess(self, p, processed, checkbox_save_to_redis, host, port, db, password, prefix):
+    def postprocess(self, p, processed, checkbox_save_to_redis, host, port, db, pwd, prefix):
         print(f"------------>checkbox_save_to_redis[{checkbox_save_to_redis}]")
         print(f"------------>host[{host}]")
         print(f"------------>port[{port}]")
         print(f"------------>db[{db}]")
-        print(f"------------>password[{password}]")
+        print(f"------------>pwd[{pwd}]")
         print(f"------------>prefix[{prefix}]")
         collection = get_collection(host, port, db, password) if checkbox_save_to_redis else None
         if collection is None:
